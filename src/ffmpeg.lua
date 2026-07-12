7141f9b53947c120e29d76b9ef5831731cda6ebf
PassiveLemon <lemonl3mn@protonmail.com>
2026-07-11 21:10:48 -0400

    refactor: path finding

--- src/ffmpeg.lua
+++ src/ffmpeg.lua
@@ -2,43 +2,39 @@ local ssh = require("ssh")
 
 local ffmpeg = { }
 
--- Get the ffmpeg path based on the ffmpeg arguments
-function ffmpeg.get_ffmpeg(args)
-  -- '^(dir)$'
-  local dir = args[0]:match("^([/%w]+)$")
-  return dir
-end
-
 -- Get the source path based on the ffmpeg arguments
 function ffmpeg.get_source(args_str)
-  -- '-i /(dir)/file'
-  local dir = args_str:match("%-i ([/%w]+)/")
+  -- '-i file:"(/dir/file)"'
+  local path = args_str:match('%-i%s+file:"([^"]+)"')
+  -- '"(/dir)/file"'
+  local dir = path:match('(.+)/[^/]+$')
   return dir
 end
 
 -- Get the destination path based on the ffmpeg arguments
-function ffmpeg.get_dest(args_str)
-  -- '-o /(dir)/file'
-  local dir = args_str:match("%-o ([/%w]+)/")
+function ffmpeg.get_dest(args_str, slug)
+  slug = "%-hls_segment_filename"
+  -- '<slug> "(/dir/file)"'
+  local path = args_str:match(slug .. '%s+"([^"]+)"')
+  -- '"(/dir)/file"'
+  local dir = path:match('(.+)/[^/]+$')
   return dir
 end
 
 -- Rewrite the destination path and return all ffmpeg args
 function ffmpeg.rewrite_dest(cfg, args)
-  -- Concat all args into a string
-  local args_str = ""
-  for _, arg in ipairs(args) do
-    args_str = args_str .. " " .. arg
-  end
+  -- Remove cmd and concat other args to get our flags
+  args[0] = nil
+  local args_str = " " .. table.concat(args, " ")
   -- Get ffmpeg paths
-  local fmpg = ffmpeg.get_ffmpeg(args)
   local src = ffmpeg.get_source(args_str)
   local dest = ffmpeg.get_dest(args_str)
   -- Rewrite paths
-  local rewrite = args_str:gsub(fmpg, ""):gsub(src, cfg.source_dir):gsub(dest, cfg.dest_dir)
+  local rewrite = args_str:gsub(src, cfg.source_dir):gsub(dest, cfg.dest_dir)
   return rewrite
 end
 
+-- The ffmpeg command to run
 function ffmpeg.cmd(cfg, args)
   local cmd = cfg.ffmpeg_path
   local flags = ffmpeg.rewrite_dest(cfg, args)
