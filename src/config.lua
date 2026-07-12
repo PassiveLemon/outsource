d9890e8f0d1f8a22f0ad6f46d1b53c0627ce32be
PassiveLemon <lemonl3mn@protonmail.com>
2026-07-11 21:11:03 -0400

    refactor: config ssh flags

--- src/config.lua
+++ src/config.lua
@@ -1,17 +1,18 @@
 local config = { }
 
 config.ssh_path = os.getenv("ANTENNA_SSH_PATH") or "/usr/bin/ssh"
-config.ssh_flags = os.getenv("ANTENNA_SSH_FLAGS") or "-i " .. os.getenv("HOME") .. "/.ssh/id_ed25519"
+local default_ssh_flags = "-i " .. os.getenv("HOME") .. "/.ssh/id_ed25519 " .. os.getenv("ANTENNA_SSH_HOST")
+config.ssh_flags = os.getenv("ANTENNA_SSH_FLAGS") or default_ssh_flags
 
 config.ffmpeg_path = os.getenv("ANTENNA_FFMPEG_PATH") or "/usr/bin/ffmpeg"
 
 config.source_dir = os.getenv("ANTENNA_SOURCE_DIR") or ""
 config.dest_dir = os.getenv("ANTENNA_DEST_DIR") or ""
 
-for k, v in ipairs(config) do
-  if (v == "") or (not v) then
+for k, v in pairs(config) do
+  if (v == "") then
     print("Error: " .. k .. " was not defined")
-    os.exit(1, true)
+    os.exit(1)
   end
 end
 
