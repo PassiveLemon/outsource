0544cb361a95b3168e1a89a77bb982922d792697
PassiveLemon <lemonl3mn@protonmail.com>
2026-08-04 15:36:34 -0400

    feature: remove ffmpeg hardware flags
    If the remote client fails with hardware, a host without hardware will also fail. This allows for software fallback

--- src/ffmpeg.lua
+++ src/ffmpeg.lua
@@ -3,9 +3,11 @@ local ssh = require("ssh")
 
 local posix = require("posix")
 
-local ffmpeg = { }
+local ffmpeg = {
+  hardware_flags = { "-init_hw_device", "-filter_hw_device" },
+}
 
--- Rewrite paths and return all ffmpeg args
+-- Rewrite paths and return all FFmpeg args
 function ffmpeg.rewrite_paths(cfg, args)
   for path_map in cfg.map_dirs:gmatch("[^;]+") do
     local from = path_map:match('^(.-)//')
@@ -13,6 +15,23 @@ function ffmpeg.rewrite_paths(cfg, args)
     log.debug("Mapping '" .. from .. "' to '" .. to .. "'")
     for i, flag in ipairs(args) do
       args[i] = flag:gsub(from, to)
+    end
+  end
+  return args
+end
+
+-- Remove hardware flags from FFmpeg args
+function ffmpeg.no_hardware(_, args)
+  -- Detect the flag first, then remove it and it's value
+  for k, _ in pairs(ffmpeg.hardware_flags) do
+    for i, flag in ipairs(args) do
+      local tack = args[i]
+      local value = args[i+1]
+      if flag:match(k) then
+        log.debug("Removing hardware flag: '" .. tack .. " " .. value .. "'")
+        table.remove(args, (i+1))
+        table.remove(args, i)
+      end
     end
   end
   return args
@@ -32,7 +51,7 @@ function ffmpeg.local_ffmpeg(cmd, args)
   return true
 end
 
--- The ffmpeg command to run
+-- The FFmpeg command to run
 function ffmpeg.cmd(cfg, args)
   args[0] = nil
   log.info("[" .. cfg.mode .. "] Received '" .. table.concat(args, " ") .. "'")
@@ -46,6 +65,7 @@ function ffmpeg.cmd(cfg, args)
   local session = ssh.cmd(cfg, cmd, flags)
   if not session then
     log.warn("Remote FFmpeg command failed, running locally...")
+    flags = ffmpeg.no_hardware(cfg, args)
     session = ffmpeg.local_ffmpeg(cmd, flags)
   end
   return session
